8a396507467eff721622bf25613c38ce4663d9cc
mahdigithubr <mahdialhasan5020@gmail.com>
2024-12-06 21:39:34 +0600

    sorry i contineu no pushing. complete the projects

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+function handelKeyboardKeyPress(event) {
+  const playerPress = event.key;
+  if (playerPress === "Escape") {
+    gameOver();
+  }
+  const currentAlphabetElement = document.getElementById("alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+
+  if (playerPress === expectedAlphabet) {
+    const currentScore = getValueById("current-score");
+    const newScore = currentScore + 1;
+    setTextValueById("current-score", newScore);
+    showElementById(expectedAlphabet, "bg-orange-400");
+    continueGame();
+  } else {
+    const currentLife = getValueById("current-life");
+    const newLife = currentLife - 1;
+    setTextValueById("current-life", newLife);
+    if (newLife === 0) {
+      console.log("Game over");
+      gameOver();
+    }
+  }
+}
+
+document.addEventListener("keyup", handelKeyboardKeyPress);
+
 function continueGame() {
   const alphabet = getRandomAlphabets();
   const displayAlphabet = document.getElementById("alphabet");
@@ -7,6 +35,21 @@ function continueGame() {
 
 function playNow() {
   hiddenElementById("home-screen");
-  showElementById("playground-section");
+  hiddenElementById("final-score");
+  showElementById("playground-section", "hidden");
+  setTextValueById("current-score", 0);
+  setTextValueById("current-life", 5);
   continueGame();
+}
+
+function gameOver() {
+  hiddenElementById("playground-section");
+  showElementById("final-score", "hidden");
+  // set a last score
+  const currentScore = getValueById("current-score");
+  setTextValueById("last-score", currentScore);
+  // remove last alphabet background color
+  const currentAlphabet = getElementById("alphabet");
+  console.log(currentAlphabet);
+  showElementById(currentAlphabet, "bg-orange-400");
 }
--- js/utility.js
+++ js/utility.js
@@ -3,14 +3,26 @@ function hiddenElementById(elementId) {
   element.classList.add("hidden");
 }
 
-function showElementById(elementId) {
+function showElementById(elementId, className) {
   const element = document.getElementById(elementId);
-  element.classList.remove("hidden");
+  element.classList.remove(className);
 }
 
 function setBackgroundColor(elementId) {
   const element = document.getElementById(elementId);
   element.classList.add("bg-orange-400");
+}
+
+function getValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValue = element.innerText;
+  const value = parseInt(elementValue);
+  return value;
+}
+
+function setTextValueById(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
 }
 
 function getRandomAlphabets() {
@@ -20,4 +32,10 @@ function getRandomAlphabets() {
   const index = Math.round(randomNumber);
   const alphabet = alphabets[index];
   return alphabet;
+}
+
+function getElementById(elementId) {
+  const element = document.getElementById(elementId);
+  const value = element.innerText;
+  return value;
 }
